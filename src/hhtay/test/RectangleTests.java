19c1704b94242e146a3ec344db24dffe2afb6989
hhtay <hhtay@palo-it.com>
2018-01-24 15:23:39 +0800

    development

--- src/hhtay/test/RectangleTests.java
+++ src/hhtay/test/RectangleTests.java
@@ -1,5 +1,16 @@
 package hhtay.test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import hhtay.Rectangle;
+
 public class RectangleTests {
 
+	Rectangle rect = new Rectangle(5,4);
+	
+	@Test
+	public void testGetArea() {
+		assertEquals(rect.getVolume(), 20);
+	}
 }
